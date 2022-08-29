ed3ae2a8697c9f2ecdf37a82be02041f9c2bd255
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-08-29 15:01:00 +0100

    Remove fanstatic directory

--- ckanext/fork/plugin.py
+++ ckanext/fork/plugin.py
@@ -3,12 +3,10 @@ import ckan.plugins.toolkit as toolkit
 
 
 class ForkPlugin(plugins.SingletonPlugin):
+
     plugins.implements(plugins.IConfigurer)
 
     # IConfigurer
-
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('fanstatic',
-            'fork')
