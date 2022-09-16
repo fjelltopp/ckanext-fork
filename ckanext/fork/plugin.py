14ff81814dcfa891b08d3393a0bb214e2fe9ea8d
Jonathan Berry <jonathan.s.berry@gmail.com>
2022-09-16 19:48:07 +0100

    ADX-860 Resource Autocomplete Action (#1)
    * ADX-860 Implement IActions
    
    * ADX-860 Draft implementation of resource_autocomplete action
    
    * ADX-860 Improve action logic
    
    * ADX-860 Test the resource_autocomplete action
    
    * ADX-860 Simple plugin test

--- ckanext/fork/plugin.py
+++ ckanext/fork/plugin.py
@@ -1,12 +1,20 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
+import ckanext.fork.actions as fork_actions
 
 
 class ForkPlugin(plugins.SingletonPlugin):
 
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IActions)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+
+    # IActions
+    def get_actions(self):
+        return {
+            'resource_autocomplete': fork_actions.resource_autocomplete
+        }
