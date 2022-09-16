701d4b843c9df142b27842850e9e66c9e7cdfff9
Jonathan S Berry <jonathan.s.berry@gmail.com>
2022-09-16 19:52:57 +0100

    ADX-865 Add fork field to resource

--- ckanext/fork/plugin.py
+++ ckanext/fork/plugin.py
@@ -1,20 +1,60 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 import ckanext.fork.actions as fork_actions
+import ckanext.fork.validators as fork_validators
 
 
-class ForkPlugin(plugins.SingletonPlugin):
+class ForkPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
 
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IActions)
+    plugins.implements(plugins.IDatasetForm)
+    plugins.implements(plugins.IValidators)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
 
+    # IDatasetForm
+    def create_package_schema(self):
+        schema = super(ForkPlugin, self).create_package_schema()
+        schema['resources'].update({
+            'fork': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_validator('valid_fork'),
+            ]
+        })
+        return schema
+
+    def update_package_schema(self):
+        schema = super(ForkPlugin, self).update_package_schema()
+        schema['resources'].update({
+            'fork': [
+                toolkit.get_validator('ignore_missing'),
+                toolkit.get_validator('valid_fork')
+            ]
+        })
+        return schema
+
+    def is_fallback(self):
+        # Return True to register this plugin as the default handler for
+        # package types not handled by any other IDatasetForm plugin.
+        return False
+
+    def package_types(self):
+        # This plugin doesn't handle any special package types, it just
+        # registers itself as the default (above).
+        return []
+
     # IActions
     def get_actions(self):
         return {
             'resource_autocomplete': fork_actions.resource_autocomplete
+        }
+
+    # IValidators
+    def get_validators(self):
+        return {
+            'valid_fork': fork_validators.valid_fork
         }
