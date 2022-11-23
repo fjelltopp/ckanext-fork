a440f1dd95930f6514bc69f2ca68f4abd1229dc2
Jonathan Berry <jonathan.s.berry@gmail.com>
2022-11-23 10:30:15 +0000

    ADX-879 Resource ID searching & ADX-882 Include private datasets (#12)
    * ADX-879 If q matches a resource ID, return that resource
    
    * ADX-879 Test resource ID searching
    
    * ADX-882 Include private datasets
    
    * ADX-882 Regression test to ensure private datasets are included

--- ckanext/fork/tests/test_actions.py
+++ ckanext/fork/tests/test_actions.py
@@ -27,11 +27,18 @@ def datasets(reset_db, reset_index):
         name=f"test-dataset-{i:02d}",
         owner_org=organization['id']
     ) for i in range(3)]
+    datasets.append(factories.Dataset(
+        title="Private Dataset",
+        name="test-dataset-04",
+        owner_org=organization['id'],
+        private=True
+    ))
 
     for (d, dataset) in enumerate(datasets):
         dataset["resources"] = [factories.Resource(
             name=f"Test Resource {(d*3)+r:02d}",
-            package_id=dataset['id']
+            package_id=dataset['id'],
+            id=f"{str(d)*8}-{str(d)*4}-{str(d)*4}-{str(d)*4}-{str(r)*12}"
         ) for r in range(3)]
 
     return datasets
@@ -46,9 +53,13 @@ class TestResourceAutocomplete():
 
     @pytest.mark.parametrize("q,result_names", [
         ('02', ['test-dataset-02', 'test-dataset-00']),
-        ('00', ['test-dataset-00', 'test-dataset-02', 'test-dataset-01']),
+        ('00', ['test-dataset-00', 'test-dataset-04', 'test-dataset-02', 'test-dataset-01']),
         ('01', ['test-dataset-01', 'test-dataset-00']),
         ('Resource 01', ['test-dataset-01', 'test-dataset-00']),
+        ('22222222-2222-2222-2222-000000000000', ['test-dataset-02']),
+        ('00000000-0000-0000-0000-111111111111', ['test-dataset-00']),
+        # Check private datasets are included in the response
+        ('Private Resource 01', ['test-dataset-04', 'test-dataset-01', 'test-dataset-00'])
     ])
     def test_resource_autocomplete(self, q, result_names, datasets):
         result = call_action('resource_autocomplete', q=q)
